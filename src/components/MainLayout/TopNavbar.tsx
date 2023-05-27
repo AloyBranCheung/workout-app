a83c55b1e770c5d86c6e4b4a3cda5b300ab80a09
bcheung <brandon.cheung.dev@gmail.com>
2023-05-27 17:38:53 -0400

    adds responsive to topnavbar

--- src/components/MainLayout/TopNavbar.tsx
+++ src/components/MainLayout/TopNavbar.tsx
@@ -4,11 +4,13 @@ import HamburgerIcon from "../UI/icons/HamburgerIcon"
 
 export default function TopNavbar() {
   return (
-    <div className="w-full bg-tertiary flex items-center justify-between px-5 py-3 border-solid border-2 border-black shadow-neobrutShadow">
-      <h4 className="text-shadow-textShadow text-white text-xl">
-        simplyworkouts
-      </h4>
-      <HamburgerIcon />
+    <div className="border-solid border-2 border-black shadow-neobrutShadow bg-tertiary flex items-center justify-center">
+      <div className="w-full max-w-7xl flex items-center justify-between px-5 py-3">
+        <h4 className="text-shadow-textShadow text-white text-xl">
+          simplyworkouts
+        </h4>
+        <HamburgerIcon />
+      </div>
     </div>
   )
 }
