f559c2136d5fc1a00bc0d192858b8a3668e2ba74
bcheung <brandon.cheung.dev@gmail.com>
2023-05-27 20:32:31 -0400

    wip

--- src/components/MainLayout/PopupMenu.tsx
+++ src/components/MainLayout/PopupMenu.tsx
@@ -1,5 +1,74 @@
-import React from "react"
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import React, { useMemo } from "react"
+import { AnimatePresence, motion } from "framer-motion"
+// types/utils
+import { OffsetPosition, Position } from "src/types/PopupMenuTypes"
+import pxToRem from "src/utils/pxToRem"
 
-export default function PopupMenu() {
-  return <div>PopupMenu</div>
+export interface PopupMenuProps {
+  isOpen: boolean
+  children: React.ReactNode
+  anchorEl: React.MutableRefObject<HTMLDivElement | null>
+  position: Position
+  popupTopOffset?: number
+}
+
+export default function PopupMenu({
+  isOpen,
+  children,
+  anchorEl,
+  position,
+  popupTopOffset,
+}: PopupMenuProps) {
+  const offsetPosition: OffsetPosition = useMemo(() => {
+    if (!anchorEl.current) {
+      const defaultOffset = {
+        topOffset: 0,
+        leftOffset: 0,
+      }
+
+      return {
+        [position]: defaultOffset,
+      }
+    }
+    const { bottom, right, x } = anchorEl.current.getBoundingClientRect()
+
+    return {
+      [Position.BottomLeft]: {
+        topOffset: bottom + popupTopOffset!,
+        leftOffset: 0,
+      },
+      [Position.BottomCenter]: {
+        topOffset: bottom + popupTopOffset!,
+        leftOffset: (right - x) / 2 + x / 2,
+      },
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [anchorEl.current, position, popupTopOffset])
+
+  return (
+    <div
+      className={`absolute`}
+      style={{
+        top: pxToRem(offsetPosition[position].topOffset),
+        left: pxToRem(offsetPosition[position].leftOffset),
+      }}
+    >
+      <AnimatePresence>
+        {isOpen && (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            {children}
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  )
+}
+
+PopupMenu.defaultProps = {
+  popupTopOffset: 0,
 }
--- src/components/MainLayout/TopNavbar.tsx
+++ src/components/MainLayout/TopNavbar.tsx
@@ -1,20 +1,32 @@
-import React, { useState } from "react"
+import React, { useState, useRef } from "react"
 // components
 import HamburgerIcon from "../UI/icons/HamburgerIcon"
 import PopupMenu from "./PopupMenu"
+// types
+import { Position } from "src/types/PopupMenuTypes"
 
 export default function TopNavbar() {
+  const anchorEl = useRef<HTMLDivElement | null>(null)
   const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   return (
-    <div className="border-solid border-2 border-black shadow-neobrutShadow bg-tertiary flex items-center justify-center relative">
+    <div
+      ref={anchorEl}
+      className="border-solid border-2 border-black shadow-neobrutShadow bg-tertiary flex items-center justify-center"
+    >
       <div className="w-full max-w-7xl flex items-center justify-between px-5 py-3">
         <h4 className="text-shadow-textShadow text-white text-xl">
           simplyworkouts
         </h4>
         <HamburgerIcon onClick={() => setIsMenuOpen(!isMenuOpen)} />
       </div>
-      <PopupMenu />
+      <PopupMenu
+        position={Position.BottomCenter}
+        anchorEl={anchorEl}
+        isOpen={isMenuOpen}
+      >
+        <div className="h-52 bg-white w-4/5">popup menu</div>
+      </PopupMenu>
     </div>
   )
 }
