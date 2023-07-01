2a534216400ae2003f9e6120a5a06e4157b13993
Brandon <brandon.cheung.dev@gmail.com>
2023-07-01 07:32:07 -0400

    wip

--- src/__tests__/jest/MainLayout.test.tsx
+++ src/__tests__/jest/MainLayout.test.tsx
@@ -2,28 +2,51 @@ import "@testing-library/jest-dom"
 import { render, screen } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 // component
-import MainLayout from "src/components/MainLayout"
+import TopNavbar from "src/components/MainLayout/TopNavbar"
 
 jest.mock("next/router", () => require("next-router-mock"))
+const mockFn = jest.fn()
 
 describe("TopNavbar", () => {
   it("renders a topnavbar", async () => {
-    render(<MainLayout>hello world</MainLayout>)
-    const user = userEvent.setup()
+    render(
+      <TopNavbar
+        isMenuOpen={false}
+        onClickLogout={mockFn}
+        onClickBrand={mockFn}
+        onClickPlan={mockFn}
+        onToggleMenu={mockFn}
+      />
+    )
 
-    // https://stackoverflow.com/questions/68865784/testing-react-components-that-get-state-setstate-properties-passed-by-parent
+    const heading = await screen.getByRole("heading", {
+      name: /simplyworkouts/i,
+    })
+    const hamburgerIcon = await screen.getByRole("img", {
+      name: /navbar-menu-icon/i,
+    })
+
+    expect(heading).toBeInTheDocument()
+    expect(hamburgerIcon).toBeInTheDocument()
+  })
+
+  it("shows dropdown menu", async () => {
+    render(
+      <TopNavbar
+        onClickLogout={mockFn}
+        onClickBrand={mockFn}
+        onClickPlan={mockFn}
+        onToggleMenu={mockFn}
+        isMenuOpen={true}
+      />
+    )
+    const user = userEvent.setup()
+    await hamburgerIcon = await screen.getByRole('img', {  name: /navbar\-menu\-icon/i})
+    
+    // expect mockFn toHaveBeenCalled
+
+    const logoutBtn = screen.getByRole("button", { name: /logout/i })
 
     screen.debug()
-    // const heading = screen.getByRole("heading", {
-    //   name: /simplyworkouts/i,
-    // })
-    // const hamburgerIcon = screen.getByRole("img", {
-    //   name: /navbar-menu-icon/i,
-    // })
-
-    // expect(heading).toBeInTheDocument()
-    // expect(hamburgerIcon).toBeInTheDocument()
-
-    // await user.click(hamburgerIcon)
   })
 })
