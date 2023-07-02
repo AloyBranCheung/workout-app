fa15bab44103ca54b3ceb8d6b7595f30dbd98358
bcheung <brandon.cheung.dev@gmail.com>
2023-07-02 13:00:17 -0400

    updates linting error

--- src/mocks/workouts.ts
+++ src/mocks/workouts.ts
@@ -5,7 +5,7 @@ export const MOCK_WORKOUT_PLANS: GetWorkoutPlansOutput = new Array(3)
   .map((_, index) => ({
     id: index,
     name: `Workout Plan ${index + 1}`,
-    lastWorkout: 1688172643 * 1000, // in milliseconds
+    lastWorkout: "1688172643 * 1000", // in milliseconds
     duration: 3600, // seconds
     updatedAt: "12312",
     createdAt: "12312",
