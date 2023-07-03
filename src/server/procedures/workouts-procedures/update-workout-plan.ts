2e9a9f0ab8b9db8fe11a5025555bf717405d62b9
bcheung <brandon.cheung.dev@gmail.com>
2023-07-03 18:08:56 -0400

    adds update workout plan

--- src/server/procedures/workouts-procedures/update-workout-plan.ts
+++ src/server/procedures/workouts-procedures/update-workout-plan.ts
@@ -5,8 +5,6 @@ import prisma from "src/utils/prisma"
 const updateWorkoutPlan = tProtectedProcedure
   .input(UpdatePlanSchema)
   .mutation(async ({ input: { name, exercises, exerciseOrder, planId } }) => {
-    console.log("exercises", exercises)
-    console.log("exerciseorder", exerciseOrder)
     const updatedWorkoutPlan = await prisma.workoutPlan.update({
       where: {
         planId,
@@ -18,7 +16,37 @@ const updateWorkoutPlan = tProtectedProcedure
         },
       },
     })
-    return { updatedWorkoutPlan }
+
+    // get targetId to update from exercises object
+    const targetHash: { [targetId: string]: (typeof exercises)[string] } = {}
+    const targetIdArr = []
+    for (const exerciseId of Object.keys(exercises)) {
+      const exerciseObj = exercises[exerciseId]
+      if (!(exerciseObj.targetId in targetHash)) {
+        targetHash[exerciseObj.targetId] = exerciseObj
+        targetIdArr.push(exerciseObj.targetId)
+      }
+    }
+
+    // use Promise.all to update all targets
+    const updateTarget = async (targetId: string) => {
+      const updatedTarget = await prisma.target.update({
+        where: {
+          targetId,
+        },
+        data: {
+          targetReps: Number(targetHash[targetId].reps),
+          targetSets: Number(targetHash[targetId].sets),
+        },
+      })
+      return updatedTarget
+    }
+
+    const updatedTargets = await Promise.all(
+      targetIdArr.map((targetId) => updateTarget(targetId))
+    )
+
+    return { updatedWorkoutPlan, updatedTargets }
   })
 
 export default updateWorkoutPlan
