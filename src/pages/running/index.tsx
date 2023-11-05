deec7164a6c251b329fbcb6dbcf03e85abc226f0
bcheung <brandon.cheung.dev@gmail.com>
2023-11-04 22:26:13 -0400

    adds layout for running page

--- src/pages/running/index.tsx
+++ src/pages/running/index.tsx
@@ -1,5 +1,10 @@
-import React from "react"
+import { ReactElement } from "react"
+import MainLayout from "src/components/MainLayout"
 
 export default function RunningPage() {
   return <div>RunningPage</div>
+}
+
+RunningPage.getLayout = function getLayout(page: ReactElement) {
+  return <MainLayout>{page}</MainLayout>
 }
