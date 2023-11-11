f5013d07a4d1fd2cd59a38514e38ff916f2e7129
bcheung <brandon.cheung.dev@gmail.com>
2023-11-11 09:32:26 -0500

    adds active exercise scrolling

--- src/components/CurrActiveSesh/index.tsx
+++ src/components/CurrActiveSesh/index.tsx
@@ -1,8 +1,28 @@
-import React from "react"
+import React, { useState, useMemo } from "react"
 // hooks
 import useCurrActiveSesh from "src/hooks/useCurrActiveSesh"
 import useGetSessionById from "src/hooks/useGetSessionById"
+// lodash
+import { capitalize } from "lodash"
+// components
 import LoadingSpinner from "../UI/LoadingSpinner"
+import Carousel from "../UI/Carousel"
+import SecondaryCard from "../UI/SecondaryCard"
+import Text, { Typography } from "../UI/typography/Text"
+
+interface Exercise {
+  userId: string
+  createdAt: string
+  updatedAt: string
+  name: string
+  url: string | null
+  description: string | null
+  gymId: string
+  unit: string
+  exerciseId: string
+  targetReps: number | null
+  targetSets: number | null
+}
 
 export default function CurrActiveSeshContainer() {
   const { currActiveSeshId, isLoading: isCurrActiveSeshLoading } =
@@ -12,7 +32,72 @@ export default function CurrActiveSeshContainer() {
 
   const isLoading = isCurrActiveSeshLoading || isSessionLoading
 
-  console.log(sessionRes)
+  const [currActiveExercise, setCurrActiveExercise] = useState<Exercise>()
 
-  return isLoading ? <LoadingSpinner /> : <div>CurrActiveSeshContainer</div>
+  const exerciseHashmap = useMemo(() => {
+    const exercisesArr = sessionRes?.workoutPlan?.exercises
+    if (!exercisesArr) return {}
+
+    const hash: { [key: string]: Exercise } = {}
+    for (const exercise of exercisesArr) {
+      if (!(exercise.exerciseId in hash)) {
+        hash[exercise.exerciseId] = exercise
+      }
+    }
+    return hash
+  }, [sessionRes?.workoutPlan?.exercises])
+
+  const exercisesList = useMemo(() => {
+    const exerciseOrderArr = sessionRes?.workoutPlan?.exerciseOrder
+    if (!exerciseOrderArr) return []
+    const exerciseOrderObjArr = []
+    for (const exerciseId of exerciseOrderArr) {
+      exerciseOrderObjArr.push(exerciseHashmap[exerciseId])
+    }
+    return exerciseOrderObjArr
+  }, [exerciseHashmap, sessionRes?.workoutPlan?.exerciseOrder])
+
+  const carouselItems = useMemo(() => {
+    const list = exercisesList.map((exercise) => ({
+      id: exercise.exerciseId,
+      value: (
+        <SecondaryCard className="px-10 py-10 text-center">
+          <b>{exercise.name}</b>
+        </SecondaryCard>
+      ),
+    }))
+
+    const defaultFirstExercise = exerciseHashmap[list[0]?.id]
+    if (defaultFirstExercise) {
+      setCurrActiveExercise(exerciseHashmap[list[0].id])
+    }
+    return list
+  }, [exerciseHashmap, exercisesList])
+
+  return isLoading ? (
+    <LoadingSpinner />
+  ) : (
+    <div className="flex flex-col gap-2">
+      <div className="w-full">
+        <Carousel
+          items={carouselItems}
+          onSlideChange={(swiper) => {
+            if (!swiper) return
+            const newCurrActiveExercise = carouselItems[swiper.activeIndex].id
+
+            if (newCurrActiveExercise) {
+              setCurrActiveExercise(exerciseHashmap[newCurrActiveExercise])
+            }
+          }}
+        />
+      </div>
+      <Text
+        text={
+          currActiveExercise?.name ? capitalize(currActiveExercise.name) : ""
+        }
+        typography={Typography.h3}
+        className="text-h3"
+      />
+    </div>
+  )
 }
--- src/components/UI/Carousel.tsx
+++ src/components/UI/Carousel.tsx
@@ -1,18 +1,49 @@
 import "swiper/css"
 import "swiper/css/navigation"
 import "swiper/css/pagination"
-import React from "react"
+import React, { useState } from "react"
 import { Swiper, SwiperSlide } from "swiper/react"
+import SwiperCore from "swiper"
 import { Navigation, Pagination } from "swiper/modules"
 
 interface CarouselProps {
   items: { id: string | number; value: React.ReactNode }[]
+  onSlideChange?: (swiper: SwiperCore | null) => void
+  navigation?: boolean
+  pagination?: boolean
 }
 
-export default function Carousel({ items }: CarouselProps) {
+export default function Carousel({
+  items,
+  navigation,
+  pagination,
+  onSlideChange,
+}: CarouselProps) {
+  const [swiperApi, setSwiperApi] = useState<SwiperCore | null>(null)
+
   const swiperSlideWrap = items.map((item) => (
     <SwiperSlide key={item.id}>{item.value}</SwiperSlide>
   ))
 
-  return <Swiper modules={[Navigation, Pagination]}>{swiperSlideWrap}</Swiper>
+  return (
+    <Swiper
+      onSwiper={(swiper) => {
+        setSwiperApi(swiper)
+      }}
+      onSlideChange={() => {
+        if (onSlideChange) onSlideChange(swiperApi)
+      }}
+      navigation={navigation}
+      modules={[Navigation, Pagination]}
+      pagination={{ clickable: true, enabled: pagination }}
+    >
+      {swiperSlideWrap}
+    </Swiper>
+  )
+}
+
+Carousel.defaultProps = {
+  navigation: true,
+  pagination: true,
+  onSlideChange: undefined,
 }
