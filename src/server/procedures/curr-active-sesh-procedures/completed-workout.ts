39294ba8e52c01ea575b62af069bdf02686de7a6
bcheung <brandon.cheung.dev@gmail.com>
2023-11-25 15:22:35 -0500

    adds backend

--- src/server/procedures/curr-active-sesh-procedures/completed-workout.ts
+++ src/server/procedures/curr-active-sesh-procedures/completed-workout.ts
@@ -6,8 +6,44 @@ import { CompletedSetsSchema } from "src/validators/curr-active-exercises-schema
 const completedWorkout = tProtectedProcedure
   .input(CompletedSetsSchema)
   .mutation(async ({ input, ctx: { user } }) => {
-    console.log("input", input)
-    return "Ok"
+    try {
+      const sessionId = input[0].sessionId
+      // close session (add end duration)
+      await prisma.session.update({
+        where: {
+          sessionId,
+        },
+        data: {
+          endDuration: new Date().toISOString(),
+        },
+      })
+
+      // update Set table
+      const setsToInput = input.map((obj) => ({
+        weight: Number(obj.weight),
+        unit: obj.unit,
+        reps: Number(obj.reps),
+        sessionId,
+        exerciseId: obj.exerciseId,
+      }))
+
+      await prisma.set.createMany({
+        data: setsToInput,
+      })
+
+      // close curractivesesh (delete by userid)
+      await prisma.currActiveSesh.delete({
+        where: {
+          userId: user.id,
+        },
+      })
+    } catch (error) {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Error completing workout.",
+        cause: error,
+      })
+    }
   })
 
 export default completedWorkout
